83a49d19fc51b6f61371315641133a38a843ae96
Prashant varma <prashantvarma5083@gmail.com>
2023-03-23 14:45:11 +0530

    Image section added

--- src/component/Form.js
+++ src/component/Form.js
@@ -1,7 +1,23 @@
 import React from "react";
 import "./Form.css"
+import memesData from "../memeData"
+import { useState } from 'react';
+
 
 export default function Form(){
+    const [memeImage, setmemeImage] = useState("");
+    // let url;
+    function getMemeImage() {
+        const memesArray = memesData.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        //  url = memesArray[randomNumber].url
+        // console.log(url)
+        if(memeImage == ""){
+            
+        }
+        setmemeImage(memesArray[randomNumber].url);
+    }
+    
     return (
         <>  
             <section className="section">
@@ -9,7 +25,10 @@ export default function Form(){
                     <input type="text" placeholder="Shut up" />
                     <input type="text" placeholder="and take my money" />
                 </div>
-                <button className="btn">Get a new meme image 📷</button>
+                <button className="btn" onClick={getMemeImage}>Get a new meme image 📷</button>
+                <div className="meme">
+                   <img src={memeImage} alt="df"  className="meme--image"/>
+                </div>
             </section>
         </>
     )
